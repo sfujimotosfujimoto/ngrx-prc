ef955dfb5789c0158426bb2eeb83a33921254e9a
S Fujimoto <unicoshun@gmail.com>
2017-01-27 22:48:51 +0900

    19

---
+++ src/app/store/store-data.ts
@@ -0,0 +1,15 @@
+
+import {Participant} from '../../../shared/model/participant';
+import {Message} from '../../../shared/model/message';
+import {Thread} from '../../../shared/model/thread';
+
+export interface StoreData {
+
+  participants: {[key: number]: Participant};
+
+  threads: {[key: number]: Thread};
+
+  messages: {[key: number]: Message};
+
+
+}
---
+++ src/app/thread-section/thread-section.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {ThreadsService} from '../services/threads.service';
+import {Store} from '@ngrx/store';
+import {ApplicationState} from '../store/application-state';
+import {LoadUserThreadsAction} from '../store/actions';
+
+
+@Component({
+  selector: 'thread-section',
+  templateUrl: './thread-section.component.html',
+  styleUrls: ['./thread-section.component.css']
+})
+export class ThreadSectionComponent implements OnInit {
+
+  constructor(private threadsService: ThreadsService,
+              private store: Store<ApplicationState>) {
+    store.subscribe(
+      console.log
+    );
+
+  }
+
+
+
+  ngOnInit() {
+
+        this.threadsService.loadUserThreads()
+          .subscribe(
+            allUserData => this.store.dispatch(
+              new LoadUserThreadsAction()
+            )
+          );
+
+  }
+
+}
